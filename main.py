b81648903658f6c1740d0b46d8d1bc55b1d11032
joancolmenares03 <142939033+joancolmenares03@users.noreply.github.com>
2024-08-12 15:40:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import funciones
 
 def main():
     funciones.elementosRepetidos([11, 56, 22])
-    #funciones.elementosRepetidos([11, 56, 22, 22])
-    #funciones.vocalesContar("Hola mundo pan")           
-    #funciones.comparador([11,56,89,74],[23,45,11,96,78,56])
-    #funciones.promedioArray([89,33,0.2,-1,4.232])
-    #funciones.mediana([11, 56, 22, 11])
+    funciones.elementosRepetidos([11, 56, 22, 22])
+    funciones.vocalesContar("Hola mundo")     
+    funciones.vocalesContar("It is what it is")      
+    funciones.comparador([11,56,89,74],[23,45,11,96,78,56])
+    funciones.promedioArray([89,33,0.2,-1,4.232])
+    funciones.mediana([11, 56, 22, 11])
+    funciones.mediana([58,97,11,35,1])
 
 if __name__ == "__main__":
     main()
